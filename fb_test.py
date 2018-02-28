cc15d7e98d90f1191ec6f8a0a8d599ac6d53e41c
Jhoana Marie Crisostomo <jhuaini@gmail.com>
2018-03-01 03:08:13 +0800

    c3

--- fb_test.py
+++ fb_test.py
@@ -22,33 +22,33 @@ class LoginTest(unittest.TestCase):
         #self = webdriver.Firefox(firefox_options=opts)
         #self.driver.set_page_load_timeout(30)
         self.driver.get("https://www.facebook.com/")
-        #self.driver.maximize_window()
+        self.driver.maximize_window()
 
     #test case method
-    #def test_login(self):
-     #   driver = self.driver
+    def test_login(self):
+        driver = self.driver
 
         #declare variables
-      #  fbusername = 'jhuanini'
-       # fbpw = 'mahalpabanyako'
-       # fbusernameid = 'email'
-       # fbpwid = 'pass'
-       # loginbuttonxpath = '//*[@id="u_0_2"]'
-       # fblogocssselector = 'span._2md'
+        fbusername = 'jhuanini'
+        fbpw = 'mahalpabanyako'
+        fbusernameid = 'email'
+        fbpwid = 'pass'
+        loginbuttonxpath = '//*[@id="u_0_2"]'
+        fblogocssselector = 'span._2md'
 
-        #fbusernameelement = WebDriverWait(driver,1).until(lambda driver: driver.find_element_by_id(fbusernameid))
-        #fbpwelement = WebDriverWait(driver,1).until(lambda driver: driver.find_element_by_id(fbpwid))
-        #loginbuttonelement = WebDriverWait(driver,1).until(lambda driver: driver.find_element_by_xpath(loginbuttonxpath))
-        #fbusernameelement.clear()
-        #fbusernameelement.send_keys(fbusername)
-        #fbpwelement.clear()
-        #fbpwelement.send_keys(fbpw)
-        #loginbuttonelement.click()
-        #fblogoclassnameelement = WebDriverWait(driver,10).until(lambda driver: driver.find_element_by_css_selector(fblogocssselector))
-        #fblogoclassnameelement.click()
+        fbusernameelement = WebDriverWait(driver,1).until(lambda driver: driver.find_element_by_id(fbusernameid))
+        fbpwelement = WebDriverWait(driver,1).until(lambda driver: driver.find_element_by_id(fbpwid))
+        loginbuttonelement = WebDriverWait(driver,1).until(lambda driver: driver.find_element_by_xpath(loginbuttonxpath))
+        fbusernameelement.clear()
+        fbusernameelement.send_keys(fbusername)
+        fbpwelement.clear()
+        fbpwelement.send_keys(fbpw)
+        loginbuttonelement.click()
+        fblogoclassnameelement = WebDriverWait(driver,10).until(lambda driver: driver.find_element_by_css_selector(fblogocssselector))
+        fblogoclassnameelement.click()
 
-    #def tearDown(self):
-     #   self.driver.quit()
+    def tearDown(self):
+        self.driver.quit()
 
 
 if __name__ == '__main__':
